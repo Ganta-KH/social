1b33471d9100f92f07eb418717d0d10c9a5b4d1e
Khalil Bentaiba <khalilbentaiba@gmail.com>
2023-12-13 23:08:43 +0100

    add button for new post

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,23 +1,14 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Dashboard') }}</div>
+    <div class="container">
+        <form action="" method="get">
+            <input type="submit" value="  +  " class="add-post">
+        </form>
 
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
-
-                    {{ __('You are logged in!') }}
-                </div>
-            </div>
-        </div>
+        {{-- @foreach ($posts as $post)
+            <h1>hi</h1>
+            <img src="{{ asset($post->image) }}" alt="img">
+        @endforeach --}}
     </div>
-</div>
 @endsection
