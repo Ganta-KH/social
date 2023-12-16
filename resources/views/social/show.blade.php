d6fac633297a40451b40013c18b1a18e11ddb4c4
Khalil Bentaiba <khalilbentaiba@gmail.com>
2023-12-16 18:06:22 +0100

    style the show post page

--- resources/views/social/show.blade.php
+++ resources/views/social/show.blade.php
@@ -2,19 +2,30 @@
 
 @section('content')
     <div class="show">
-        <img src="{{ asset($post->image) }}">  
-        <div class="func">
-            <div class="func-1">
-                <a href=""><svg class="like" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><title>thumb-up-outline</title><path d="M5,9V21H1V9H5M9,21A2,2 0 0,1 7,19V9C7,8.45 7.22,7.95 7.59,7.59L14.17,1L15.23,2.06C15.5,2.33 15.67,2.7 15.67,3.11L15.64,3.43L14.69,8H21C22.11,8 23,8.9 23,10V12C23,12.26 22.95,12.5 22.86,12.73L19.84,19.78C19.54,20.5 18.83,21 18,21H9M9,19H18.03L21,12V10H12.21L13.34,4.68L9,9.03V19Z"/></svg></a>
-            </div>
-            <div class="func-2">
-                <a href=""><svg class="edit" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><title>image-edit-outline</title><path d="M22.7 14.3L21.7 15.3L19.7 13.3L20.7 12.3C20.8 12.2 20.9 12.1 21.1 12.1C21.2 12.1 21.4 12.2 21.5 12.3L22.8 13.6C22.9 13.8 22.9 14.1 22.7 14.3M13 19.9V22H15.1L21.2 15.9L19.2 13.9L13 19.9M11.21 15.83L9.25 13.47L6.5 17H13.12L15.66 14.55L13.96 12.29L11.21 15.83M11 19.9V19.05L11.05 19H5V5H19V11.31L21 9.38V5C21 3.9 20.11 3 19 3H5C3.9 3 3 3.9 3 5V19C3 20.11 3.9 21 5 21H11V19.9Z" /></svg></a>
-                <a href=""><svg class="delete" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><title>delete-outline</title><path d="M6,19A2,2 0 0,0 8,21H16A2,2 0 0,0 18,19V7H6V19M8,9H16V19H8V9M15.5,4L14.5,3H9.5L8.5,4H5V6H19V4H15.5Z" /></svg></a>
+
+        <div class="post">
+            <img src="{{ asset($post->image) }}">
+
+            <div class="func">
+                <div class="func-1">
+                    <a href=""><svg class="like" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><title>thumb-up-outline</title><path d="M5,9V21H1V9H5M9,21A2,2 0 0,1 7,19V9C7,8.45 7.22,7.95 7.59,7.59L14.17,1L15.23,2.06C15.5,2.33 15.67,2.7 15.67,3.11L15.64,3.43L14.69,8H21C22.11,8 23,8.9 23,10V12C23,12.26 22.95,12.5 22.86,12.73L19.84,19.78C19.54,20.5 18.83,21 18,21H9M9,19H18.03L21,12V10H12.21L13.34,4.68L9,9.03V19Z"/></svg></a>
+                </div>
+                <div class="func-2">
+                    <form action="{{ route('social.edit', $post->id) }}" method="get" enctype="multipart/form-data">
+                        <input type="image" src="/images/icons/edit.svg">
+                    </form>
+                    <form action="{{ route('social.delete', $post->id) }}" method="post">
+                        @csrf
+                        @method('delete')
+                        <input type="image" src="/images/icons/delete.svg">
+                    </form>
+                </div>
             </div>
         </div>
 
-        <div class="cmnts">
-            
+        <div class="comments">
+
         </div>
+        
     </div>    
 @endsection
