0be44627b2c20e68e84571974311d9751d8c04f4
Khalil Bentaiba <khalilbentaiba@gmail.com>
2023-12-19 17:22:58 +0100

    add profile infromation the the index page

--- resources/views/social/index.blade.php
+++ resources/views/social/index.blade.php
@@ -1,13 +1,33 @@
 @extends('layouts.app')
 
 @section('content')
+    <div class="p-info navbar navbar-expand-md navbar-light bg-white shadow-sm">
+        <img src="{{ asset($user->avatar) }}" alt="">
+        <div class="p-details">
+            <div class="username-edit">
+                <h1><strong>{{ $user->username }}</strong></h1>
+                <form action="" method="get">
+                    <input type="submit" name="edit-p" class="edit-p" value="Edit profile">
+                </form>
+            </div>
+            <div class="details">
+                <div class="numbers">
+                    <h5>{{ $posts->count() }} <strong>posts</strong></h5>
+                    <h5>{{ $posts->count() }} <strong>followers</strong></h5>
+                    <h5>{{ $posts->count() }} <strong>following</strong></h5>
+                </div>
+                <p>description</p>
+            </div>
+        </div>
+    </div>
+
     <div class="home">
         @foreach ($posts as $post)
             <div class="card shadow-sm">
                 <form action="{{ route('social.show', $post) }}" method="get">
                     <input type="image" src="{{ asset($post->image) }}">
                 </form>
-            
+
                 <div class="func">
                     <div class="func-1">
                         <a href=""><svg class="like" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><title>thumb-up-outline</title><path d="M5,9V21H1V9H5M9,21A2,2 0 0,1 7,19V9C7,8.45 7.22,7.95 7.59,7.59L14.17,1L15.23,2.06C15.5,2.33 15.67,2.7 15.67,3.11L15.64,3.43L14.69,8H21C22.11,8 23,8.9 23,10V12C23,12.26 22.95,12.5 22.86,12.73L19.84,19.78C19.54,20.5 18.83,21 18,21H9M9,19H18.03L21,12V10H12.21L13.34,4.68L9,9.03V19Z"/></svg></a>
